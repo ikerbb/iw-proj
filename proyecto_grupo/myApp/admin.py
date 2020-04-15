0d441f7147fd7b9ae7121361da3596e25125611a
Unknown <ikerbb@opendeusto.es>
2020-04-15 17:38:55 +0200

    15/04

--- proyecto_grupo/myApp/admin.py
+++ proyecto_grupo/myApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Empleado,Tarea,Cliente,Proyecto
 
 # Register your models here.
+
+admin.site.register(Empleado)
+admin.site.register(Tarea)
+admin.site.register(Cliente)
+admin.site.register(Proyecto)
--- proyecto_grupo/myApp/models.py
+++ proyecto_grupo/myApp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+CHOICE_estado_empleado = [(0, 'Fijo'), (1, 'Eventual')]
+CHOICE_prioridad = [(0, 'Alta'), (1, 'Media'), (2, 'Baja')]
+CHOICE_estado_tarea = [(0, 'Sin empezar'), (1, 'En marcha'), (2, 'Acabada')]
+
+class Empleado(models.Model):
+    dni = models.CharField(max_length=9)
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    telefono = models.IntegerField()
+    estado = models.BooleanField(choices=CHOICE_estado_empleado)
+
+class Tarea(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=1000)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    responsable = models.ManyToManyField(Empleado)
+    prioridad = models.CharField(max_length=100, choices=CHOICE_prioridad)
+    estado = models.CharField(max_length=100, choices=CHOICE_estado_tarea)
+    notas = models.CharField(max_length=10000)
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    empresa = models.CharField(max_length=100)
+    telefono = models.IntegerField()
+    email = models.EmailField(max_length=100)
+    datos_adicionales = models.CharField(max_length=1000)
+
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=1000)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    presupuesto = models.IntegerField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    tareas = models.ManyToManyField(Tarea)
+    empleados = models.ManyToManyField(Empleado)
