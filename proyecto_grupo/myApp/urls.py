811bf904eb7269b69a2bce47bf423bf573564658
Unknown <ikerbb@opendeusto.es>
2020-04-16 17:45:51 +0200

    primeras views

--- proyecto_grupo/myApp/urls.py
+++ proyecto_grupo/myApp/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('empleados/', views.EmpleadosListView.as_view(), name='empleadosListView'),
+    path('empleados/<int:pk>/', views.EmpleadosDetailView.as_view(), name='empleadosDetailView'),
+    path('proyectos/', views.ProyectosListView.as_view(), name='proyectosListView'),
+    path('proyectos/<int:pk>/', views.ProyectosDetailView.as_view(), name='proyectosDetailView'),
+    path('tareas/', views.TareasListView.as_view(), name='tareasListView'),
+    path('tareas/<int:pk>/', views.TareasDetailView.as_view(), name='tareasDetailView'),
+    path('clientes/', views.ClientesListView.as_view(), name='clientesListView'),
+    path('clientes/<int:pk>/', views.ClientesDetailView.as_view(), name='clientesDetailView')
 ]
--- proyecto_grupo/myApp/views.py
+++ proyecto_grupo/myApp/views.py
@@ -3,6 +3,85 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.views.generic import DetailView, ListView
+from .models import Proyecto, Empleado, Tarea, Cliente
 
 def index(request):
     return HttpResponse("Texto de prueba")
+
+class EmpleadosListView(ListView):
+    model = Empleado
+    template_name = "empleadosListView.html"
+    queryset = Empleado.objects.order_by('nombre')
+
+    def get_context_data(self, **kwargs):
+        context = super(EmpleadosListView, self).get_context_data(**kwargs)
+        #anadir context['dato'] = 'informacion' que es lo que irá a la plantilla
+        return context
+
+class EmpleadosDetailView(DetailView):
+    model = Empleado
+    template_name = "empleadosDetailView.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(EmpleadosDetailView, self).get_context_data(**kwargs)
+        # anadir context['dato'] = 'informacion' que es lo que irá a la plantilla
+        return context
+
+class ProyectosListView(ListView):
+    model = Proyecto
+    template_name = "proyectosListView.html"
+    queryset = Proyecto.objects.order_by('nombre')
+    #cuando hagamos filtros cambiaremos esto para traer solo X proyectos, o ordenados por lo que sea
+
+    def get_context_data(self, **kwargs):
+        context = super(ProyectosListView, self).get_context_data(**kwargs)
+        #anadir context['dato'] = 'informacion' que es lo que irá a la plantilla
+        return context
+
+class ProyectosDetailView(DetailView):
+    model = Proyecto
+    template_name = "proyectosDetailView.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProyectosDetailView, self).get_context_data(**kwargs)
+        #anadir context['dato'] = 'informacion' que es lo que irá a la plantilla
+        return context
+
+class TareasListView(ListView):
+    model = Tarea
+    template_name = "tareasListView.html"
+    queryset = Tarea.objects.order_by('nombre')
+
+    def get_context_object_name(self, **kwargs):
+        context = super(TareasListView, self).get_context_object_name(**kwargs)
+        #anadir context['dato'] = 'informacion' que es lo que irá a la plantilla
+        return context
+
+class TareasDetailView(DetailView):
+    model = Tarea
+    template_name = "tareasDetailView.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(TareasDetailView, self).get_context_data(**kwargs)
+        #anadir context['dato'] = 'informacion' que es lo que irá a la plantilla
+        return context
+
+class ClientesListView(ListView):
+    model = Cliente
+    template_name = "clientesListView.html"
+    queryset = Cliente.objects.order_by('nombre')
+
+    def get_context_data(self, **kwargs):
+        context = super(ClientesListView, self).get_context_data(**kwargs)
+        #anadir context['dato'] = 'informacion' que es lo que irá a la plantilla
+        return context
+
+class ClientesDetailView(DetailView):
+    model = Cliente
+    template_name = "clientesDetailView.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ClientesDetailView, self).get_context_data(**kwargs)
+        #anadir context['dato'] = 'informacion' que es lo que irá a la plantilla
+        return context
