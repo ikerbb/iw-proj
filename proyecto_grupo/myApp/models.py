828eea70d1636bc72350aa7550a84fbd16712a85
Ainara11 <ainara11.lopez@opendeusto.es>
2020-04-28 18:22:04 +0200

    doc.

--- proyecto_grupo/myApp/models.py
+++ proyecto_grupo/myApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 #Clase para los empleados, donde se establecen las características a rellenar de los empleados que se incluyan.
-#Cada dato tendrá un tamaño máximo a incluir, excepto el telefono del empleado.
+#Cada dato tendrá una longitud máxima a incluir, excepto el telefono del empleado.
 #Al final mostrará por pantalla el id, nombre y apellidos del empleado.
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
@@ -26,7 +26,7 @@ class Empleado(models.Model):
         return f'{self.id}, {self.nombre}, {self.apellidos}'
 
 #Clase para las tareas, donde se establecen las características a rellenar de las tareas que se incluyan.
-#Todos los datos de las tareas tendrán un tamaño máximo, excluyendo las fechas de inicio y de fin.
+#Todos los datos de las tareas tendrán una longitud máxima, excluyendo las fechas de inicio y de fin.
 #Al final mostrará por pantalla el id y el nombre de la tarea.
 class Proyecto(models.Model):
     nombre = models.CharField(max_length=100)
@@ -56,13 +56,13 @@ class Tarea(models.Model):
         return f'{self.id}, {self.nombre_tarea}'
 
 #Clase para los clientes, donde se establecen las características a rellenar de todos los clientes que se incluyan.
-#Todos los datos de los clientes tendrán un tamaño máximo a incluir, excepto el teléfono.
+#Todos los datos de los clientes tendrán una longitud máxima a incluir, excepto el teléfono.
 #Al final, mostrará por pantalla el id, nombre y la empresa del cliente.
 
 
 
 #Clase para los proyectos, donde se establecen las características a rellenar de todos los proyectos que se incluyan.
-#Todos los datos de los clientes tendrán un tamaño máximo a incluir, excepto las fechas y el presupuesto.
+#Todos los datos de los clientes tendrán una longitud máxima a incluir, excepto las fechas y el presupuesto.
 #Al final, se mostrará por pantalla el id y el nombre de los proyectos.
 
 
