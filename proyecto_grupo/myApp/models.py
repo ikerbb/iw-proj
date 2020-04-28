c5611a5da51c562bde7813cfd74ff443c63b1fd4
Ainara11 <ainara11.lopez@opendeusto.es>
2020-04-28 18:08:17 +0200

    documentación

--- proyecto_grupo/myApp/models.py
+++ proyecto_grupo/myApp/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 
+#Clase para los empleados, donde se establecen las características a rellenar de los empleados que se incluyan.
+#Cada dato tendrá un tamaño máximo a incluir, excepto el telefono del empleado.
+#Al final mostrará por pantalla el id, nombre y apellidos del empleado.
 
 class Empleado(models.Model):
     dni = models.CharField(max_length=9)
@@ -13,7 +16,9 @@ class Empleado(models.Model):
     def __str__(self):
         return f'{self.id}, {self.nombre}, {self.apellidos}'
 
-
+#Clase para las tareas, donde se establecen las características a rellenar de las tareas que se incluyan.
+#Todos los datos de las tareas tendrán un tamaño máximo, excluyendo las fechas de inicio y de fin.
+#Al final mostrará por pantalla el id y el nombre de la tarea.
 
 class Tarea(models.Model):
     nombre_tarea = models.CharField(max_length=100)
@@ -30,6 +35,9 @@ class Tarea(models.Model):
     def __str__(self):
         return f'{self.id}, {self.nombre_tarea}'
 
+#Clase para los clientes, donde se establecen las características a rellenar de todos los clientes que se incluyan.
+#Todos los datos de los clientes tendrán un tamaño máximo a incluir, excepto el teléfono.
+#Al final, mostrará por pantalla el id, nombre y la empresa del cliente.
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
@@ -40,6 +48,10 @@ class Cliente(models.Model):
 
     def __str__(self):
         return f'{self.id}, {self.nombre}, {self.empresa}'
+
+#Clase para los proyectos, donde se establecen las características a rellenar de todos los proyectos que se incluyan.
+#Todos los datos de los clientes tendrán un tamaño máximo a incluir, excepto las fechas y el presupuesto.
+#Al final, se mostrará por pantalla el id y el nombre de los proyectos.
 
 
 class Proyecto(models.Model):
